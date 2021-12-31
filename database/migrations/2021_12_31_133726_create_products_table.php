30b6664fbe071f62500d39d13bb760e00b034f84
mostafa <mostafamedht98@gmail.com>
2021-12-31 16:33:22 +0200

    add product model & migration & controller

---
+++ database/migrations/2021_12_31_133726_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('restaurant_id');
+            $table->string('name');
+            $table->decimal('price', 15, 2)->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            // Foriegn Key to returant table
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// All API here must be authenticated
+Route::group(['middleware' => 'api', 'namespace' => 'Api'], function () {
+    Route::get('restaurants', 'RestaurantController@index');
 });
