b0c099d517f08465ce5eb5d084c80b4da26a18e0
mostafa <mostafamedht98@gmail.com>
2021-12-31 20:48:29 +0200

    make checkpassword middleware to authenticate api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use GuzzleHttp\Middleware;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // All API here must be authenticated
-Route::group(['middleware' => 'api', 'namespace' => 'Api'], function () {
-    Route::get('restaurants', 'RestaurantController@index');
+Route::group(['middleware' => ['api', 'checkPassword'], 'namespace' => 'Api'], function () {
+    Route::post('restaurants', 'RestaurantController@index');
 });
