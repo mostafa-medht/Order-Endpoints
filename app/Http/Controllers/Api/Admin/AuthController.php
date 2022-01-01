fd9e48ddf80ff06e0e7f8cce0bc5cd05762fc607
mostafa <mostafamedht98@gmail.com>
2022-01-01 04:37:48 +0200

    make login & logout of admin function

---
+++ app/Http/Controllers/Api/Admin/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminLoginValidation;
+use App\Traits\GeneralTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    use GeneralTrait;
+
+    public function login(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'password' => 'required',
+                'email' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                $code = $this->returnCodeAccordingToInput($validator);
+                return $this->returnValidationError($code, $validator);
+            }
+
+            // login
+            $credentials = $request->only(['email', 'password']);
+
+            $token = Auth::guard('admin-api')->attempt($credentials);
+
+            if (!$token) {
+                return $this->returnError('', 'Invalid Credentials');
+            }
+
+            $admin = Auth::guard('admin-api')->user();
+            $admin->api_token = $token;
+            // return token
+            return $this->returnData('admin', $admin);
+        } catch (\Exception $exception) {
+            return $this->returnError($exception->getCode(), $exception->getMessage());
+        }
+    } // end of login
+
+    public function logout(Request $request)
+    {
+        $token = $request->header('auth-token');
+        if ($token) {
+            try {
+                JWTAuth::setToken($token)->invalidate();
+            } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $exception) {
+                return $this->returnError('', 'some thing went wrong');
+            }
+            return $this->returnSuccessMessage('Logged out Sussessfully');
+        } else {
+            return $this->returnError('', 'some thing went wrong');
+        }
+    } // end of logout
+} // end of controller
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 // All API here must be authenticated
-Route::group(['middleware' => ['api', 'checkPassword'], 'namespace' => 'Api'], function () {
+Route::group(['middleware' => 'api', 'namespace' => 'Api'], function () {
     Route::post('restaurants', 'RestaurantController@index');
     Route::post('restaurant/{id}', 'RestaurantController@show');
+
+    Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout')->middleware('auth.guard:admin-api');
+    });
+});
+
+// All API here must be authenticated & User Role Is Admin
+Route::group(['middleware' => ['api', 'checkPassword', 'checkAdminToken:admin-api'], 'namespace' => 'Api'], function () {
+    Route::post('restaurants', 'RestaurantController@index');
 });
