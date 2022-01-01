1fc2276c4be36c3bd96f4bd1fe40a7d04633bd1e
mostafa <mostafamedht98@gmail.com>
2022-01-01 17:16:15 +0200

    build order repository class

---
+++ app/Repositories/OrderRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Order;
+use App\Traits\GeneralTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class OrderRepository implements OrderRepositoryInterface
+{
+    use GeneralTrait;
+
+    public function all($request)
+    {
+        try {
+            $orders = Order::with('products', 'user', 'restaurant')->get();
+            if ($orders)
+                return $this->returnData("orders", $orders);
+            return $this->returnError("", "There is no result");
+        } catch (\Exception $exception) {
+            return $this->returnError("", $exception->getMessage());
+        }
+    }
+
+    public function submit($request)
+    {
+        $validator = Validator::make($request->all(), [
+            'restaurant_id' => 'required',
+            'products' => 'required|array',
+            'quantities' => 'required|array',
+        ]);
+
+        if ($validator->fails()) {
+            $code = $this->returnCodeAccordingToInput($validator);
+            return $this->returnValidationError($code, $validator);
+        }
+
+        try {
+            $order = Order::create($request->all());
+            $products = $request->input('products', []);
+            $quantities = $request->input('quantities', []);
+            for ($product = 0; $product < count($products); $product++) {
+                if ($products[$product] != '') {
+                    $order->products()->attach($products[$product], ['quantity' => $quantities[$product]]);
+                }
+            }
+            return $this->returnSuccessMessage("", "Submitted Successfully");
+        } catch (\Exception $exception) {
+            return $this->returnError("", $exception->getMessage());
+        }
+    }
+
+    public function findById($request)
+    {
+        try {
+            $order = Order::where('id', $request->orderId)
+                ->with('products', 'user', 'restaurant')
+                ->first();
+            if ($order)
+                return $this->returnData("order", $order);
+            return $this->returnError("", "There is no result");
+        } catch (\Exception $exception) {
+            return $this->returnError("", $exception->getMessage());
+        }
+    }
+
+    public function update($request)
+    {
+        $orders = Order::where('id', $request->orderId)->delete();
+        $orders->update(request()->only('name'));
+    }
+
+    public function delete($request)
+    {
+        $orders = Order::where('id', $request->orderId)->firstOrFail();
+    }
+}
---
+++ app/Repositories/OrderRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Requests\SubmitOrder;
+use Illuminate\Http\Request;
+
+interface OrderRepositoryInterface
+{
+    public function all($request);
+
+    public function submit(SubmitOrder $request);
+
+    public function findById($request);
+
+    public function update($request);
+
+    public function delete($request);
+}
