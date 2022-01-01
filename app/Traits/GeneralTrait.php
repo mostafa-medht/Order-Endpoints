9d6fdc4e9b7b1f02da864f79257b32664b355da8
mostafa <mostafamedht98@gmail.com>
2022-01-01 04:35:16 +0200

    add general validation function

--- app/Traits/GeneralTrait.php
+++ app/Traits/GeneralTrait.php
@@ -35,4 +35,52 @@ trait GeneralTrait
         ]);
     } // end of return data
 
+    //////////////////
+    public function returnValidationError($code = "E001", $validator)
+    {
+        return $this->returnError($code, $validator->errors()->first());
+    } // end of return validation error
+
+    public function returnCodeAccordingToInput($validator)
+    {
+        $inputs = array_keys($validator->errors()->toArray());
+        $code = $this->getErrorCode($inputs[0]);
+        return $code;
+    } // end of code according to input
+
+    public function getErrorCode($input)
+    {
+        if ($input == "name")
+            return 'E001';
+
+        else if ($input == "password")
+            return 'E002';
+
+        else if ($input == "email")
+            return 'E003';
+
+        else if ($input == "id")
+            return 'E004';
+
+        else if ($input == "type")
+            return 'E005';
+
+        else if ($input == "message")
+            return 'E006';
+
+        else if ($input == "rate")
+            return 'E007';
+
+        else if ($input == "price")
+            return 'E008';
+
+        else if ($input == "user_id")
+            return 'E009';
+
+        else if ($input == "message_id")
+            return 'E010';
+
+        else
+            return "";
+    } // end of error code
 } // end pf trait
